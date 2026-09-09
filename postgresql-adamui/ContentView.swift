0aee17ef26ae3202abfa069e81a75df5d06b559b
977ADAM <977team.95@gmail.com>
2026-09-10 00:33:12 +0300

    Initial Commit

---
+++ postgresql-adamui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  postgresql-adamui
+//
+//  Created by Adam Shaipov on 10.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
